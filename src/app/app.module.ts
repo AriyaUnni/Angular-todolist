9184543f478256b2e52a9b8a5fd21f32d78f453e
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-11 10:48:54 +0530

    last commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
 
 const appRoutes:Routes=[
 
@@ -23,7 +25,9 @@ const appRoutes:Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
