299ceb25f262345bd814cf614b9a9b964ba1ec10
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-11 10:32:45 +0530

    routing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes:Routes=[
+
+  {
+    path:"",component:ToDoListComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +22,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
